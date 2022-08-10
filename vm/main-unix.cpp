41f43c2d666774a4d2c1764fd01fb2f29b78994a
Dave Carlton <davec@mac.com>
2022-08-10 09:10:26 -0500

    Added code to display memory protection settings

--- vm/main-unix.cpp
+++ vm/main-unix.cpp
@@ -1,7 +1,65 @@
 #include "master.hpp"
 
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+void print_prot_bits(int prot) {
+    printf((prot & PROT_READ) == 0 ? "-" : "R");
+    printf((prot & PROT_WRITE) == 0 ? "-" : "W");
+    printf((prot & PROT_EXEC) == 0 ? "-" : "X");
+}
+void *try_mmap_jit(size_t size, int prot) {
+    int map_flags = MAP_ANON | MAP_PRIVATE | MAP_JIT;
+    printf("Try mmap with MAP_JIT: ");
+    print_prot_bits(prot);
+    void *mem = mmap(NULL, size, prot, map_flags, -1, 0);
+    if (mem == MAP_FAILED || mem == NULL) {
+        printf(" FAIL: %s", strerror(errno));
+        return NULL;
+    }
+    printf(" PASS: %p", mem);
+    return mem;
+}
+void try_mprotect(void *mem, size_t size, int prot) {
+    printf("Try mprotect: %p ", mem);
+    print_prot_bits(prot);
+    if (mprotect(mem, size, prot) != 0)
+        printf(" FAIL: %s", strerror(errno));
+    else
+        printf(" PASS");
+}
+
+void try_mmap_jit_and_mprotect(size_t size, int prot_mmap, int prot_mprotect) {
+    void *addr = try_mmap_jit(size, prot_mmap);
+    printf(" -> ");
+    if (addr) {
+        try_mprotect(addr, size, prot_mprotect);
+    }
+    putchar('\n');
+}
+
+//int main-prot(int argc, const char *argv[]) {
+//for (int prot_mmap = 0; prot_mmap < 8; prot_mmap++) {
+//for (int prot_mprotect = 0; prot_mprotect < 8; prot_mprotect++) {
+//try_mmap_jit_and_mprotect(4096, prot_mmap, prot_mprotect);
+//    }
+//  }
+//return 0;
+//}
+
 int main(int argc, char** argv) {
-  factor::init_mvm();
-  factor::start_standalone_factor(argc, argv);
-  return 0;
+    
+//    int size = 4096;
+//    int map_flags = MAP_ANON | MAP_PRIVATE | MAP_JIT;
+//    void* mem = mmap(nullptr, size, PROT_READ | PROT_WRITE | PROT_EXEC, map_flags, -1, 0);
+//    if (mem == MAP_FAILED||mem==nullptr)
+//        printf("Failed to allocate executable memory");
+//    if (mprotect(mem, size, PROT_READ | PROT_EXEC) != 0)
+//        printf("WriteProtectMemory failed!\nmprotect: %s\n", strerror(errno));
+//    return 0;
+
+    factor::init_mvm();
+    factor::start_standalone_factor(argc, argv);
+    return 0;
 }
