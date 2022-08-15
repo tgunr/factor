b3ec7579ecc783de11658757cf2a886b14381891
Dave Carlton <davec@polymicro.net>
2022-08-15 18:19:21 -0500

    Hide main

--- vm/main-unix.cpp
+++ vm/main-unix.cpp
@@ -39,16 +39,16 @@ void try_mmap_jit_and_mprotect(size_t size, int prot_mmap, int prot_mprotect) {
     putchar('\n');
 }
 
-//int main-prot(int argc, const char *argv[]) {
-//for (int prot_mmap = 0; prot_mmap < 8; prot_mmap++) {
-//for (int prot_mprotect = 0; prot_mprotect < 8; prot_mprotect++) {
-//try_mmap_jit_and_mprotect(4096, prot_mmap, prot_mprotect);
-//    }
-//  }
-//return 0;
-//}
+int main(int argc, const char *argv[]) {
+for (int prot_mmap = 0; prot_mmap < 8; prot_mmap++) {
+for (int prot_mprotect = 0; prot_mprotect < 8; prot_mprotect++) {
+try_mmap_jit_and_mprotect(4096, prot_mmap, prot_mprotect);
+    }
+  }
+return 0;
+}
 
-int main(int argc, char** argv) {
+//int main(int argc, char** argv) {
     
 //    int size = 4096;
 //    int map_flags = MAP_ANON | MAP_PRIVATE | MAP_JIT;
@@ -59,7 +59,7 @@ int main(int argc, char** argv) {
 //        printf("WriteProtectMemory failed!\nmprotect: %s\n", strerror(errno));
 //    return 0;
 
-    factor::init_mvm();
-    factor::start_standalone_factor(argc, argv);
-    return 0;
-}
+//    factor::init_mvm();
+//    factor::start_standalone_factor(argc, argv);
+//    return 0;
+//}
