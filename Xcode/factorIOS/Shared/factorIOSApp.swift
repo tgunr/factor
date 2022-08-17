2eb25a723274a08e54f954b5f7a0b014bcb54cd7
Dave Carlton <davec@polymicro.net>
2022-08-17 16:36:20 -0500

    Added back factorIOS project
    All code remains in vm folder at top level

---
+++ Xcode/factorIOS/Shared/factorIOSApp.swift
@@ -0,0 +1,16 @@
+//
+//  factorIOSApp.swift
+//  Shared
+//
+//  Created by Dave Carlton on 8/16/22.
+//
+
+import SwiftUI
+
+struct factorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
